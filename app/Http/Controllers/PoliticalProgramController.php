5c2d91cdf9fe1cddb9891fce1d80c80165078c29
Jaime <jaime.srj@gmail.com>
2015-04-06 18:16:31 +0200

    Change 'unlike' to 'dislike' on 'section' table

--- app/Http/Controllers/PoliticalProgramController.php
+++ app/Http/Controllers/PoliticalProgramController.php
@@ -55,10 +55,10 @@ class PoliticalProgramController extends Controller {
 	*/
 	public function showSection($id_political_party, $section)
 	{
-		$result1 = DB::select('select `section`, `title`, `text`, `likes`, `not_understood`, `unlikes` FROM `section` WHERE `id_political_party` = ? AND `section` = ?', array($id_political_party, $section));
+		$result1 = DB::select('select `section`, `title`, `text`, `likes`, `not_understood`, `dislikes` FROM `section` WHERE `id_political_party` = ? AND `section` = ?', array($id_political_party, $section));
 		$result2 = DB::select('select COUNT(*) as comments FROM `comment` WHERE `id_political_party` = ? AND `section` = ?', array($id_political_party, $section));
 
-		$finalResult = array("section" => $result1[0]->section, "title" => $result1[0]->title, "text" => $result1[0]->text, "likes" => $result1[0]->likes, "not_understood" => $result1[0]->not_understood, "unlikes" => $result1[0]->unlikes, "comments" => $result2[0]->comments);
+		$finalResult = array("section" => $result1[0]->section, "title" => $result1[0]->title, "text" => $result1[0]->text, "likes" => $result1[0]->likes, "not_understood" => $result1[0]->not_understood, "dislikes" => $result1[0]->dislikes, "comments" => $result2[0]->comments);
 		return response()->json($finalResult);
 	}
 
